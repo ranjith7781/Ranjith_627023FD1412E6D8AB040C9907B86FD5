c190246eb9a6d745b195ad691399a44704c787ea
ranjith7781 <144509964+ranjith7781@users.noreply.github.com>
2023-09-16 09:23:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
+class BankAccount:
+    def __init__(self,account_holder_name,account_number,balance):
+        self.__account_holder_name=account_holder_name
+        self.__account_number=account_number
+        self.__account_balance=balance
+
+    def deposite(self,amount):
+        if(amount>0):
+            self.__account_balance+=amount
+            print("deposite amount Rs:{}, and Current balance after deposite Rs:{}".format(amount,self.__account_balance))
+        else:
+            print("Invalid deposite, Please try again")
+    def withdraw(self,amount):
+        if (amount>0 and self.__account_balance>=amount):
+            self.__account_balance-=amount
+            print("Withdraw Amount Rs:{}, and Current balance after withdraw Rs:{}".format(amount,self.__account_balance))
+        else:
+            print("Invalid Withdraw Ammount. Please try again")
+
+    def checkbalance(self):
+        print("Account Holder Name :{}, Your Account Number :{}, Your Current Balance Rs:{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
 
 
-year = int(input("Enter a year : "))
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+name=input("Enter Account Holder name :")
+accno=int(input("Enter Account Number :"))
+accbal=float(input("Enter Current Balance :"))
